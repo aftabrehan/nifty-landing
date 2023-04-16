f5be474fa0f0d3adade6861ad3f1a29eba2c3e74
aftabrehan <aftabrehan52422@gmail.com>
2023-04-17 00:09:33 +0500

    Created Button and Link Component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,22 +1,38 @@
+import Link from 'next/link'
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
 
 import stl from './Button.module.scss'
 
-const Button = ({ label, onClick, customClass }) => (
-  <button onClick={onClick} className={clsx(stl.btn, customClass)}>
-    {label}
-  </button>
-)
-
-Button.defaultProps = {
-  label: 'Button',
-  onClick: () => console.log('clicked!'),
-}
+const Button = ({
+  label = 'Button',
+  link,
+  target = '_self',
+  onClick,
+  variant = 'primary',
+  customClass,
+}) =>
+  link ? (
+    <Link href={link || ''}>
+      <a target={target} className={stl.link}>
+        {label}
+      </a>
+    </Link>
+  ) : (
+    <button
+      onClick={onClick}
+      className={clsx(stl.btn, stl[variant], customClass)}
+    >
+      {label}
+    </button>
+  )
 
 Button.propTypes = {
   label: PropTypes.string,
+  link: PropTypes.string,
+  target: PropTypes.string,
   onClick: PropTypes.func,
+  variant: PropTypes.oneOf(['primary', 'secondary']),
   customClass: PropTypes.string,
 }
 
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,13 @@
+import Button from 'components/button'
 import stl from './Home.module.scss'
 
 const Home = () => (
   <div className={stl.wrapper}>
     <h1>Nifty Landing</h1>
+
+    <Button label="Contact" variant="secondary" />
+    <Button label="My account" />
+    <Button label="Home" link="/" />
   </div>
 )
 
